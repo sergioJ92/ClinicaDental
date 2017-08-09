56f5bcfff03500f3522cf6b9d7e66c9e6ea5134b
sergioJ92 <jeral.sergio@gmail.com>
2017-08-08 20:31:21 -0400

    nadbar subida

---
+++ resources/views/layouts/barraNavegacion.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+	<head>
+		
+	</head>
+	<body>
+
+		<nav>
+		    <div class="nav-wrapper">
+		      <a href="#" class="brand-logo">Clinica Dental</a>
+		      <ul id="nav-mobile" class="right hide-on-med-and-down">
+		        <li><a href="/">Home</a></li>
+		        <li><a href="pacientes">Pacientes</a></li>
+		        <li><a href="personal">Personal</a></li>
+		        <li><a href="tratamientos">Tratamientos</a></li>
+		      </ul>
+		    </div>
+	    </nav>
+	    @yield('content')		
+
+	</body>
--- resources/views/pacientes.blade.php
+++ resources/views/pacientes.blade.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	
+		
+	{!! MaterializeCSS::include_full() !!}
+        <link rel="stylesheet" href="http://yourdomain.com/materialize-css/css/materialize.min.css">
+        <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="http://yourdomain.com/materialize-css/js/materialize.min.js"></script>
 	</head>
 	<body>
-			<h1>hola soy la vista de Pacientes</h1>	
+		@extends('layouts.barraNavegacion')
+
+		@section('content')	
+
+				<h1>hola soy la vista de Pacientes</h1>	
+		@stop
 	</body>
 </html>
