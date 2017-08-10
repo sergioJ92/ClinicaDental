1bee9e2b45ef3ad77720bce215e7389b8d26f7f2
sergioJ92 <jeral.sergio@gmail.com>
2017-08-09 20:05:48 -0400

    formulario

--- resources/views/usuario/crear.blade.php
+++ resources/views/usuario/crear.blade.php
@@ -1,9 +1,36 @@
-<!doctype html>
-	<head>
-		
-	</head>
-	<body>
+@extends('layouts.barraNavegacion')
 
-		<h1>Soy la vista del formulario para CREAR EL USUARIO</h1>	  	
-
-	</body>
+@section('content')
+	<div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+          <label for="first_name">Nombre</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate">
+          <label for="last_name">Apellido</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Contraseña</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="password" type="password" class="validate">
+          <label for="password">Repetir Contraseña</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+    </form>
+  </div>
+@stop
