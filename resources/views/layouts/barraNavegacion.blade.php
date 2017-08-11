3774786cef83d71fd8108ce83c620a810a091ec1
Jhannet <jhannet.jancko.camacho@gmail.com>
2017-08-11 07:21:59 -0400

    para navbar mobile

--- resources/views/layouts/barraNavegacion.blade.php
+++ resources/views/layouts/barraNavegacion.blade.php
@@ -4,20 +4,42 @@
         <link rel="stylesheet" href="http://yourdomain.com/materialize-css/css/materialize.min.css">
         <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
         <script src="http://yourdomain.com/materialize-css/js/materialize.min.js"></script>
+        
+        <script type="text/javascript">
+        	$(document).ready(function(){
+        		$(".button-collapse").sideNav();
+        		$(".dropdown-button").dropdown();
+        	});
+        	
+        </script>
 	</head>
 	<body>
-		<nav>
+		<nav class="nav-extended">
 		    <div class="nav-wrapper">
 		      <a href="#" class="brand-logo">Clinica Dental</a>
-		      <ul id="nav-mobile" class="right hide-on-med-and-down">
+		      
+		    </div>
+	    	<div class="nav-wrapper">
+	    	  <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+		      <ul id="nav-mobile" class="left hide-on-med-and-down">
 		        <li><a href="/">Home</a></li>
 		        <li><a href="pacientes">Pacientes</a></li>
 		        <li><a href="personal">Personal</a></li>
 		        <li><a href="tratamientos">Tratamientos</a></li>
 		      </ul>
+		      <ul class="side-nav" id="mobile-demo">
+		        <li><a href="/">Home</a></li>
+		        <li><a href="pacientes">Pacientes</a></li>
+		        <li><a href="personal">Personal</a></li>
+		        <li><a href="tratamientos">Tratamientos</a></li>
+		      </ul>
+		      <ul id="nav-mobile" class="right hide-on-med-and-down">
+		        <li><a href="personal">Iniciar sesion</a></li>
+		        <li><a href="tratamientos">Registrar usuario</a></li>
+		      </ul>
 		    </div>
-	    </nav>
 
+	    </nav>
 	    @yield('content')
 
 	</body>
